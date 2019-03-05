fefc643eeb29c272718a979b36d6879607648887
Ioannis Theodosiou - Info.nl <ioannis.theodosiou@l2144.buro.info.nl>
2019-03-05 14:49:23 +0100

    test https

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // import environmental variables from our variables.env file
 require('dotenv').config();
 const mongoose = require('mongoose');
+const https = require('https');
+const fs = require('fs');
 const { port, database } = require('./config')
 
 // Connect to our Database and handle any bad connections
@@ -20,7 +22,12 @@ require('./models/Test');
 require('./models/TestSession');
 require('./models/Answer');
 
-const app = require('./app')
-app.listen(port, () => console.log(`API server started on ${port}`))
+const key = fs.readFileSync('./privatekey.pem').toString(),
+  cert = fs.readFileSync('./certificate.pem').toString(),
+  // ca = fs.readFileSync('./intermediate.crt'),
+  options = { key, cert };
 
-module.exports =  app;
+const app = require('./app')
+https.createServer(options, app.callback()).listen(port, () => console.log(`API server started on ${port}`))
+
+module.exports = https;
